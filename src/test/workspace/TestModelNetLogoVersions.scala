28ba710ccb6777f839fcfa535d06ffe531c28fed
Frank Duncan <frank@kank.net>
2014-05-01 14:30:24 -0500

    Improve/Fix unit testing

--- src/test/workspace/TestModelNetLogoVersions.scala
+++ src/test/workspace/TestModelNetLogoVersions.scala
@@ -3,15 +3,17 @@
 package org.nlogo.workspace
 
 import org.scalatest.FunSuite
-import org.nlogo.api.{ FileIO, Version, DummyParserServices }
+import org.nlogo.api.{ FileIO, Version}
 import org.nlogo.api.model.ModelReader
 import org.nlogo.util.SlowTest
+import org.nlogo.nvm.DefaultParserServices
+import org.nlogo.compile.front.FrontEnd
 
 class TestModelNetLogoVersions extends FunSuite with SlowTest {
   val paths = ModelsLibrary.getModelPaths ++ ModelsLibrary.getModelPathsAtRoot("extensions")
   for(path <- paths)
     test(path) {
-      val version = ModelReader.parseModel(FileIO.file2String(path), new DummyParserServices()).version
+      val version = ModelReader.parseModel(FileIO.file2String(path), new DefaultParserServices(FrontEnd)).version
       assert(Version.compatibleVersion(version))
     }
 }
